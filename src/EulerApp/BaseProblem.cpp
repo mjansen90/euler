c47fc492f776dc79c47daf5a63e005072e56f788
Michael Jansen <mjansen90@gmail.com>
2014-12-10 21:05:47 -0800

    Add timing code to BaseProblem

--- src/EulerApp/BaseProblem.cpp
+++ src/EulerApp/BaseProblem.cpp
@@ -6,6 +6,7 @@
 
 #include "BaseProblem.h"
 
+#include <chrono>
 #include <iostream>
 
 // Constructor
@@ -26,8 +27,14 @@ void BaseProblem::Run()
    std::cout << "--------------------------------" << std::endl
              << "-- Executing: " << ProblemName().c_str() << std::endl
              << "--------------------------------" << std::endl;
+   std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
    Execute();
+   std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
    std::cout << "--------------------------------" << std::endl
              << "-- Finished: " << ProblemName().c_str() << std::endl
+             << "-- Execution Time: " << std::chrono::duration_cast<std::chrono::nanoseconds>(t2 - t1).count()    << " ns" << std::endl
+             << "-- Execution Time: " << std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count()   << " us" << std::endl
+             << "-- Execution Time: " << std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count()   << " ms" << std::endl
+             << "-- Execution Time: " << std::chrono::duration_cast<std::chrono::seconds>(t2 - t1).count()        << " s"  << std::endl
              << "--------------------------------" << std::endl << std::endl;
 }

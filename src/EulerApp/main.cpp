b3d4a2135ae60f6c8cb45b6ed146f39e76d58802
Michael Jansen <mjansen90@gmail.com>
2014-12-10 18:35:14 -0800

    Solved Problem 2

--- src/EulerApp/main.cpp
+++ src/EulerApp/main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 #include "Problem1.h"
+#include "Problem2.h"
 
 int main(int argc, char* argv[])
 {
    Problem1().Run();
+   Problem2().Run();
 
    system("pause");
    return 0;
